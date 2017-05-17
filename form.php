7a44d8bc2be7f9b43dbe3d15e43f1a7e6d090f6d
Atena Shad <atenadsh@Gmail.com>
2017-05-17 09:13:25 +0200

    initial commit

---
+++ form.php
@@ -0,0 +1,25 @@
+<?php
+
+$recipe = $_POST;
+$new_array = [];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="hidden" id="" name="recipeId" value="">
+        Name:<br> 
+        <input type="text" name="name"><br>
+        Recipe:<br>
+        <input type="text" name="recipe"><br>
+        <input type="submit" value="save">
+    </form>
+</body>
+</html>
